d46251bcfe0c8bad155b81a64c850c055752282d
salindah <h.salinda@gmail.com>
2020-06-25 00:59:07 +0200

    Added side drawer

--- burgers/src/components/Layout/Layout.js
+++ burgers/src/components/Layout/Layout.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.css';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerOpenHandler = () => {
+        this.setState({showSideDrawer: true});
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar clicked={this.sideDrawerOpenHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
--- burgers/src/components/Navigation/Toolbar/Toolbar.js
+++ burgers/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
 
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
-            ...
+        <div onClick={props.clicked}>MENU</div>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems/>
         </nav>
     </header>
 );
