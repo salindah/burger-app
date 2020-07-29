95fc7e6a7172a644a565c2b0720545170fdef22a
salindah <h.salinda@gmail.com>
2020-07-30 00:39:26 +0200

    Adding Orders page and configure routing of it to Navigation Bar.

--- burgers/src/containers/Checkout/ContactData/ContactData.js
+++ burgers/src/containers/Checkout/ContactData/ContactData.js
@@ -23,7 +23,7 @@ class ContactData extends Component {
         this.setState({ loading: true });
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.totalPrice,
+            price: this.props.price,
             customer: {
                 name: 'Salinda Hettiarachchi',
                 address: {
